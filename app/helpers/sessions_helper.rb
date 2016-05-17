9c345e2762b1c2a640af7d6b8371140920176b92
StreamAlbert <albert.tian@papaly.com>
2016-05-17 16:13:09 -0700

    User can only edit/update his own pages.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,6 +42,11 @@ module SessionsHelper
     @current_user = nil
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
