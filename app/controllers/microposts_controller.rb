0d14c00fdf6c1ce8fad5b9ff5c45de14c05cd871
StreamAlbert <albert.tian@papaly.com>
2016-06-08 13:55:48 -0700

    micro post deletion and integration test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,23 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    # This corresponds to HTTP_REFERER, as defined by the specification for HTTP. 
+    # Note that “referer” is not a typo—the word is actually misspelled in the spec. 
+    # Rails corrects this error by writing “referrer” instead.
+    # request.referrer is the previous URL, using request.referrer we arrange to redirect back to the page issuing the delete request
+    redirect_to request.referrer || root_url
   end
 
   private 
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
